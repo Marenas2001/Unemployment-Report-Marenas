5c236d1903c027b17d3f3170b4fb3129601081e9
Marenas2001 <ma1956@georgetown.edu>
2022-12-03 14:39:29 -0500

    Display latest data

--- app/unemployment.py
+++ app/unemployment.py
@@ -1,19 +1,31 @@
 
-# app/unemployment_report.py file
+# this is the "app/unemployment.py" file...
 
 import os
 import json
 from pprint import pprint
 
 import requests
+from dotenv import load_dotenv # <--- ADDITION
+
+load_dotenv() # <--- ADDITION
 
 API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
-
 
 request_url = f"https://www.alphavantage.co/query?function=UNEMPLOYMENT&apikey={API_KEY}"
 
 response = requests.get(request_url)
 
 parsed_response = json.loads(response.text)
-print(type(parsed_response))
-pprint(parsed_response)
+#print(type(parsed_response))
+#pprint(parsed_response)
+
+# Challenge A
+#
+# What is the most recent unemployment rate? And the corresponding date?
+# Display the unemployment rate using a percent sign.
+
+#breakpoint()
+
+latest = parsed_response["data"][0]
+print(latest)
